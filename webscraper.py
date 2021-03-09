913bc8f716ec399f5cd7eed84e6531875f1dc6ea
AnselRausis <76424443+AnselRausis@users.noreply.github.com>
2021-03-09 11:07:46 -0800

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -2,9 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 
 senate_URL = " https://www.Senate.gov"
-senate_page = requests.get(senate_URL)
+senate_page = requests.get(senate_URL).text
 
-senateSoup = BeautifulSoup(senate_page, 'html.parser')
-
-
-
+senateSoup = BeautifulSoup(senate_page, 'lxml')
+print(senateSoup.prettify())
